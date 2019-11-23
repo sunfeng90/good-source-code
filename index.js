964e28498007cf7004c6bb79ff9201892506cfc0
feng Sun <luckysunfeng@gmail.com>
2019-11-23 11:47:17 +0800

    添加数组去重的方法

--- index.js
+++ index.js
@@ -58,3 +58,97 @@ function monthName(date) { // 获取当前几月份
   date = date ? moment(date) : moment();
   return date.format('MMM');
 }
+
+// 随机生成字母和数字的组合(可以用于生成用户的临时编号,例如微信号)
+const randomStr = Math.random().toString(36).substr(2);
+console.log(randomStr); // 88479nv1auj
+
+// 转换布尔值
+const isTrue = !0;
+const isFalse = !1;
+const alseFalse = !!0;
+console.log(isTrue) // true
+console.log(isFalse) // false 
+console.log(alseFalse) // false
+
+// 转换成数字
+let numberOne = '100';
+numberOne = +numberOne;
+console.log(numberOne); // 100
+let numberTwo = '2222';
+numberTwo = ~~numberTwo;
+console.log(numberTwo); // 2222
+
+// 浮点数转整数
+const intNum = 100.5 | 0;
+console.log(intNum); // 100
+const intNumTwo = ~~12.55;
+console.log(intNumTwo); // 12
+
+// 数组降纬度
+let arrOne = [[2, 3], [4, 99], [88, 33]]; // 二维
+arrOne = Array.prototype.concat.apply([], arrOne);
+console.log(arrOne); // [ 2, 3, 4, 99, 88, 33 ]
+let arrTwo = [[66, 99], [33], [122]]; // 二维
+arrTwo = arrTwo.flat(2);
+console.log(arrTwo); // [ 66, 99, 33, 122 ]
+let arrThree = [[1], [2], [3], [[[4]]]]; // 多维
+console.log(arrThree.flat(Infinity)); // [ 1, 2, 3, 4 ]
+
+// 判断小数是否相等
+function equal(num1, num2) {
+  return Math.abs(num1 - num2) < Math.pow(2, -52);
+}
+console.log(equal(0.1+0.2, 0.3)); // true
+
+// 数组去重
+// 方法一:Set
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+}
+const arrFour = [1, 3, 5, 3, 6, 88, 1, 6, 11, 33, 2, 6];
+console.log(arrFour.unique()); // [1,  3,  5, 6, 88, 11, 33, 2]
+
+// 方法二:Map
+Array.prototype.unique = function() {
+  const tmp = new Map();
+  return this.filter(item => {
+    return !tmp.has(item) && tmp.set(item, 1);
+  })
+}
+const arrFive = [10, 32, 52, 32, 66, 88, 11, 6, 11, 33, 2, 66];
+console.log(arrFive.unique()); // [10, 32, 52, 66, 88, 11,  6, 33,  2]
+
+// 方法三:indexOf
+Array.prototype.unique = function () {
+  return this.filter((item, index) => {
+    return this.indexOf(item) === index;
+  })
+}
+const arrSix = [4, 5, 6, 4, 6, 11, 99, 88, 5];
+console.log(arrSix.unique()); // [ 4, 5, 6, 11, 99, 88 ]
+
+// 方法四:includes
+Array.prototype.unique = function () {
+  const newArr = [];
+  this.forEach(item => {
+    if (!newArr.includes(item)) {
+      newArr.push(item);
+    }
+  });
+  return newArr; 
+}
+const arrSeven = [114, 115, 116, 114, 116, 1111, 1199, 1188, 115];
+console.log(arrSeven.unique()); // [ 114, 115, 116, 1111, 1199, 1188 ]
+
+// 方法五:reduce
+Array.prototype.unique = function () {
+  return this.sort().reduce((init, current) => {
+    if (init.length === 0 || init[init.length - 1] !== current) {
+      init.push(current);
+    }
+    return init;
+  }, []);
+}
+const arrEight = [224, 225, 226, 224, 226, 2211, 2299, 2288, 225];
+console.log(arrEight.unique()); // [ 2211, 224, 225, 226, 2288, 2299 ]
